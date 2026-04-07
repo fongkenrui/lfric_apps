ac79cbb9965c8660fcb6fb3a3ab7c9aa2f68703d
Fong Ken Rui <kenrui.fong@gmail.com>
2026-04-07 17:00:54 +0100

    remove bad comma

--- interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
+++ interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
@@ -230,7 +230,7 @@ module conv_comorph_kernel_mod
          arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, WTHETA),                   &! detrain_down
          arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, W3),                       &! massflux_up_half
          arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, W3),                       &! gen_massflux_up
-         arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, W3),                       &! gen_massflux_down
+         arg_type(GH_FIELD,  GH_REAL,    GH_READWRITE, W3)                        &! gen_massflux_down
         /)
     integer :: operates_on = DOMAIN
   contains
