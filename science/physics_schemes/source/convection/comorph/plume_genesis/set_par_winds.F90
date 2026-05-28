642e5527a52b3ac6e0093ea76ee5b9662e861fc9
Fong Ken Rui <kenrui.fong@gmail.com>
2026-05-28 11:58:53 +0100

    Test write/flush

--- science/physics_schemes/source/convection/comorph/plume_genesis/set_par_winds.F90
+++ science/physics_schemes/source/convection/comorph/plume_genesis/set_par_winds.F90
@@ -138,10 +138,13 @@ call raise_warning(routinename, &
 ! Set parcel core winds
 do i_field = i_wind_u, i_wind_w
   do ic = 1, n_points
-    call raise_warning(routinename, &
+    ! Write to PET channel
+    write(10, *) &
       "par_gen_core shape: "//trim(adjustl(str(shape(par_gen_core))))//", fields_k shape: "//trim(adjustl(str(shape(fields_k))))&
-      //", turb_pert_k shape: "//trim(adjustl(str(shape(turb_pert_k))))))
-    call raise_fatal(routinename, "Terminate before setting core winds")
+      //", turb_pert_k shape: "//trim(adjustl(str(shape(turb_pert_k))))
+    write(10, *) "turb_pert_k_l_bound_2="//trim(adjustl(str(lbound(turb_pert_k,2))))//", turb_pert_k_u_bound="//trim(adjustl(str(ubound(turb_pert_k,2))))
+    call flush(10) ! Flush out PET channel
+    
     par_gen_core(ic,i_field) = fields_k(ic,i_field)                            &
                              + factor * turb_pert_k(ic,i_field)
   end do
