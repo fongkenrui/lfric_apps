65d2c8466a50e7e807a5f0bada336fcdf25fabb0
Fong Ken Rui <kenrui.fong@gmail.com>
2026-06-04 16:44:45 +0100

    gate the pc2 settings so that pc2_homog_conv_pressure does not trigger with diagnostic comorph

--- interfaces/physics_schemes_interface/source/support/um_physics_init_mod.f90
+++ interfaces/physics_schemes_interface/source/support/um_physics_init_mod.f90
@@ -1072,8 +1072,9 @@ contains
     ! Flags for diagnostic output are set here
     call set_convection_output_flags( )
     ! Check the contents of the convection parameters module
-    ! Note that if l_param_conv is false the checks won't run
-    call check_run_convection()
+    ! Note: this check is fairly aggressive and will complain if conv params are set even though
+    ! l_param_conv is off
+    ! call check_run_convection()
 
     ! ----------------------------------------------------------------
     ! UM convection scheme settings for plume scavenging of UKCA
@@ -1170,7 +1171,7 @@ contains
       case(scheme_pc2)
         i_cld_vn                     = i_cld_pc2
         dbsdtbs_turb_0               = real( dbsdtbs_turb_0_in, r_um )
-        if (cv_scheme == cv_scheme_comorph) then
+        if (cv_scheme == cv_scheme_comorph .and. l_param_conv) then
           forced_cu = forced_cu_cca
           i_pc2_homog_g_method = i_pc2_homog_g_width
           l_pc2_homog_conv_pressure = .true.
