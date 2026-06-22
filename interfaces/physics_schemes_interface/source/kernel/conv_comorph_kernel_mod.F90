99165d01e95bc6422f1bc14b3400301b54ae0efc
Fong Ken Rui <kenrui.fong@gmail.com>
2026-06-22 11:09:49 +0100

    fix wrong fs map

--- interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
+++ interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
@@ -2697,7 +2697,7 @@ contains
       if (.not. associated(turb_radius, empty_real_data) ) then
         do k = 1, n_conv_levels
           do i = 1, row_length
-            parcel_radius(map_w3(1,i) + k) = turb_radius(i,1,k)
+            parcel_radius(map_wth(1,i) + k) = turb_radius(i,1,k)
           end do
         end do
       end if
