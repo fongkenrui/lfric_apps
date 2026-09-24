11c24eb7cb538bd278ac35d7f2c6d80e212a563a
Fong Ken Rui <kenrui.fong@gmail.com>
2026-09-24 14:56:13 +0100

    move frac_ent diags inside outer loop guard

--- interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
+++ interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
@@ -2719,45 +2719,42 @@ contains
           end do
         end do
       end if
+      if (.not. associated(frac_entrain_up, empty_real_data) ) then
+        do k = 1, n_conv_levels
+          do i = 1, row_length
+            ! entrain_up = M_up (lower level) * frac_ent
+            ! Divide further by delta Z to get frac entrain rate
+            ! We keep to dimensionless frac ent for now
+            frac_entrain_up(map_wth(1,i) + k) = ent_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_um))
+          end do
+        end do
+        deallocate(ent_up)
+      end if
+      if (.not. associated(frac_entrain_down, empty_real_data) ) then
+        do k = 1, n_conv_levels
+          do i = 1, row_length
+            frac_entrain_down(map_wth(1,i) + k) = ent_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_um))
+          end do
+        end do
+        deallocate(ent_down)
+      end if
+      if (.not. associated(frac_detrain_up, empty_real_data) ) then
+        do k = 1, n_conv_levels
+          do i = 1, row_length
+            frac_detrain_up(map_wth(1,i) + k) = det_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_um))
+          end do
+        end do
+        deallocate(det_up)
+      end if
+      if (.not. associated(frac_detrain_down, empty_real_data) ) then
+        do k = 1, n_conv_levels
+          do i = 1, row_length
+            frac_detrain_down(map_wth(1,i) + k) = det_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_um))
+          end do
+        end do
+        deallocate(det_down)
+      end if
     end if ! outer_iterations
-    if (.not. associated(frac_entrain_up, empty_real_data) ) then
-      do k = 1, n_conv_levels
-        do i = 1, row_length
-          ! entrain_up = M_up (lower level) * frac_ent
-          ! Divide further by delta Z to get frac entrain rate
-          ! We keep to dimensionless frac ent for now
-          frac_entrain_up(map_wth(1,i) + k) = ent_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_um))
-        end do
-      end do
-      deallocate(ent_up)
-    end if
-
-    if (.not. associated(frac_entrain_down, empty_real_data) ) then
-      do k = 1, n_conv_levels
-        do i = 1, row_length
-          frac_entrain_down(map_wth(1,i) + k) = ent_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_um))
-        end do
-      end do
-      deallocate(ent_down)
-    end if
-
-    if (.not. associated(frac_detrain_up, empty_real_data) ) then
-      do k = 1, n_conv_levels
-        do i = 1, row_length
-          frac_detrain_up(map_wth(1,i) + k) = det_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_um))
-        end do
-      end do
-      deallocate(det_up)
-    end if
-
-    if (.not. associated(frac_detrain_down, empty_real_data) ) then
-      do k = 1, n_conv_levels
-        do i = 1, row_length
-          frac_detrain_down(map_wth(1,i) + k) = det_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_um))
-        end do
-      end do
-      deallocate(det_down)
-    end if
 
     if (l_mom) then
       do k = 1, n_conv_levels
