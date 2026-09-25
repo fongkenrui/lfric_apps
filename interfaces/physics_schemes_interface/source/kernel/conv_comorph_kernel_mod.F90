1d51bdc74394ec986e00764e60ea1588e02edad1
Fong Ken Rui <kenrui.fong@gmail.com>
2026-09-25 15:48:03 +0100

    Use an estimated pre-detrainment mass flux instead

--- interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
+++ interfaces/physics_schemes_interface/source/kernel/conv_comorph_kernel_mod.F90
@@ -2731,40 +2731,53 @@ contains
             ! entrain_up = M_up (lower level) * frac_ent
             ! Divide further by delta Z to get frac entrain rate
             ! We keep to dimensionless frac ent for now
-            frac_entrain_up(map_wth(1,i) + k) = ent_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_um))
+
+            !if (abs(ent_up(i,1,k)) > up_flux_half(i,1,k) ) then 
+              ! Guard against pathological cases where up_flux_half is zero but ent_up is nonzero...
+              !frac_entrain_up(map_wth(1,i) + k) = 0.0_r_def
+            !end if
+            
+            ! Estimate pre-detrainment mass-flux for the denominator
+            frac_entrain_up(map_wth(1,i) + k) = ent_up(i,1,k) / max(up_flux_half(i,1,k) &
+                                                + det_up(i,1,k), tiny(1.0_r_um))
             ! Write output
             write(10, *) "ent_up=", ent_up(i,1,k)
+            write(10, *) "det_up=", det_up(i,1,k)
             write(10, *) "up_flux_half=", up_flux_half(i,1,k)
             write(10, *) "frac_entrain_up=", frac_entrain_up(map_wth(1,i) + k)
             flush(10)
           end do
         end do
-        deallocate(ent_up)
       end if
       if (.not. associated(frac_entrain_down, empty_real_data) ) then
         do k = 1, n_conv_levels
           do i = 1, row_length
-            frac_entrain_down(map_wth(1,i) + k) = ent_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_def))
+            frac_entrain_down(map_wth(1,i) + k) = ent_down(i,1,k) / max(down_flux_half(i,1,k) &
+                                                  + det_down(i,1,k), tiny(1.0_r_um))
           end do
         end do
-        deallocate(ent_down)
       end if
       if (.not. associated(frac_detrain_up, empty_real_data) ) then
         do k = 1, n_conv_levels
           do i = 1, row_length
-            frac_detrain_up(map_wth(1,i) + k) = det_up(i,1,k) / max(up_flux_half(i,1,k), tiny(1.0_r_def))
+            frac_detrain_up(map_wth(1,i) + k) = det_up(i,1,k) / max(up_flux_half(i,1,k) &
+                                                + det_up(i,1,k), tiny(1.0_r_um))
           end do
         end do
-        deallocate(det_up)
       end if
       if (.not. associated(frac_detrain_down, empty_real_data) ) then
         do k = 1, n_conv_levels
           do i = 1, row_length
-            frac_detrain_down(map_wth(1,i) + k) = det_down(i,1,k) / max(down_flux_half(i,1,k), tiny(1.0_r_def))
+            frac_detrain_down(map_wth(1,i) + k) = det_down(i,1,k) / max(down_flux_half(i,1,k) &
+                                                  + det_down(i,1,k), tiny(1.0_r_um))
           end do
         end do
-        deallocate(det_down)
       end if
+      ! Deallocate ent/det arrays
+      deallocate(ent_up)
+      deallocate(ent_down)
+      deallocate(det_up)
+      deallocate(det_down)
     end if ! outer_iterations
 
     if (l_mom) then
